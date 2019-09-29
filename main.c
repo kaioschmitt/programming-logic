1c23d5af58b5657693dace0b22960b07bea95fcf
Pedro Queiroz <54639269+pmqueiroz@users.noreply.github.com>
2019-09-28 23:20:32 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -48,60 +48,172 @@ main(){
         break;
     }
 
+    // PRIMEIRA QUESTAO
+
     question1:
-    printf("Finge que a questao 1 ta aq");
+    int st_number, nd_number;
+
+    printf("Digite o primeiro numero\n");
+    scanf("%d", &st_number);
+    printf("Digite o segundo numero\n");
+    scanf("%d", &nd_number);
+
+    if (st_number > nd_number) {
+        printf("O numero %d eh maior\n", st_number);
+    }
+    else {
+        if (st_number < nd_number){
+            printf("O numero %d eh maior\n", nd_number);
+        }
+        else
+        {
+            printf("Os numeros sao iguais\n");
+        }
+    }
     system ("pause");
     goto inicio;
+
+    //SEGUNDA QUESTAO
+
     question2:
-    printf("Finge que a questao 2 ta aq");
+    int st_number, nd_number;
+
+    printf("Digite o primeiro numero\n");
+    scanf("%d", &st_number);
+    printf("Digite o segundo numero\n");
+    scanf("%d", &nd_number);
+
+    if (st_number > nd_number) {
+        printf("O numero %d eh maior\n", st_number);
+    }
+    else {
+        if (st_number < nd_number){
+            printf("O numero %d eh maior\n", nd_number);
+        }
+        else
+        {
+            printf("Os numeros sao iguais\n");
+        }
+    }
     system ("pause");
     goto inicio;
+
+    //TERCEIRA QUESTAO
+
     question3:
-    printf("Finge que a questao 3 ta aq");
+    int st_number, nd_number, modulus_test;
+
+    printf("Enter the first number\n");
+    scanf("%d", &st_number);
+    printf("Enter the second number\n");
+    scanf("%d", &nd_number);
+
+    modulus_test = st_number + nd_number;
+
+    if (modulus_test % 2 == 0) {
+        printf("The number %d is even\n", modulus_test);
+    }
+    else {
+        printf("The number %d is odd\n", modulus_test);
+    }
     system ("pause");
     goto inicio;
+
+    //QUARTA QUESTAO
+
     question4:
-    printf("Finge que a questao 4 ta aq");
+    int st_number, nd_number;
+
+    printf("Enter the first number\n");
+    scanf("%d", &st_number);
+    printf("Enter the second number\n");
+    scanf("%d", &nd_number);
+
+    //show the first number multiplied by 3
+    printf("First result: %d\n", st_number*3);
+    //show the second number multiplied by 4
+    printf("Second result: %d", nd_number*4);
     system ("pause");
     goto inicio;
+
+    //QUINTA QUESTAO
+
     question5:
-    printf("Finge que a questao 5 ta aq");
+    nt number;
+
+    printf("Insert a number\n");
+    scanf("%d", &number);
+
+    if (number >= 0) {
+        printf("The number is positive\n");
+    }
+    else {
+        printf("The number is negative");
     system ("pause");
+    }
     goto inicio;
+
+    //SEXTA QUESTAO
+
     question6:
-    printf("Finge que a questao 6 ta aq");
+    int number;
+
+    printf("Insert a number\n");
+    scanf("%d", &number);
+
+    if (number > 10){
+        printf("The number is greater than 10\n");
+    }
+    else
+        if (number < 10){
+            printf("The number is less than 10\n");
+        }
+        else{
+            printf("The number is equal to 10");
+        }
     system ("pause");
     goto inicio;
+
+    //SETIMA QUESTAO
+
     question7:
-    printf("Finge que a questao 7 ta aq");
+    float st_grade, nd_grade, rd_grade, average;
+
+    printf("Insert the first value\n");
+    scanf("%f", &st_grade);
+    printf("Insert the second value\n");
+    scanf("%f", &nd_grade);
+    printf("Insert the third value\n");
+    scanf("%f", &rd_grade);
+
+    average = (st_grade + nd_grade + rd_grade)/3;
+
+    if (average >= 7){
+        printf("Approved");
+    }
+    else{
+        printf("Failed");
+    }
     system ("pause");
     goto inicio;
+
+    //OITAVA QUESTAO
+
     question8:
-    printf("Finge que a questao 8 ta aq");
+    int st_number, nd_number;
+
+    printf("Insert the first value\n");
+    scanf("%d", &st_number);
+    printf("Insert the second value\n");
+    scanf("%d", &nd_number);
+
+    if (st_number >= nd_number){
+        printf("%d - %d", st_number,nd_number);
+    }
+    else{
+        printf("%d - %d", nd_number,st_number);
+    }
     system ("pause");
     goto inicio;
 
 }
-
-
-
-seu if ta assim:
-
-if ()
-    /* code */
-else
-    /* code */
-
-
-e deveria esta assim:
-
-if ()
-{
-    /* code */
-}
-else
-{
-    /* code */
-}
-
-
