36a323e2469321d6630e3fb56838aacd019137f4
Pedro Queiroz <54639269+pmqueiroz@users.noreply.github.com>
2019-10-15 14:37:08 -0300

    Add files via upload

--- while_loop/while_loop#01.c
+++ while_loop/while_loop#01.c
@@ -1,12 +1,49 @@
 #include <stdio.h>
 
 main(){
-
-    int i;
-    i = 1;
-
-    while (i <= 10){
-        printf("%d\n" ,i);
-        i = i+1;
+    system("title students average with while");
+    int loop;
+    loop = 1;
+    float st_grade, nd_grade, average;
+    
+    while (loop <= 10){
+        switch (loop){
+        case 1:
+            printf("Insert the first grade of the %dst stundent: ", loop);
+            scanf("%f", &st_grade);
+            printf("Insert the second grade of the %dst stundent: ", loop);
+            scanf("%f", &nd_grade);
+            average = (st_grade + nd_grade)/2;
+            printf("The average is %.1f\n\n", average);
+            loop = loop+1;
+            break;
+        case 2:
+            printf("Insert the first grade of the %dnd stundent: ", loop);
+            scanf("%f", &st_grade);
+            printf("Insert the second grade of the %dnd stundent: ", loop);
+            scanf("%f", &nd_grade);
+            average = (st_grade + nd_grade)/2;
+            printf("The average is %.1f\n\n", average);
+            loop = loop+1;
+            break;
+        case 3:
+            printf("Insert the first grade of the %drd stundent: ", loop);
+            scanf("%f", &st_grade);
+            printf("Insert the second grade of the %drd stundent: ", loop);
+            scanf("%f", &nd_grade);
+            average = (st_grade + nd_grade)/2;
+            printf("The average is %.1f\n\n", average);
+            loop = loop+1;
+            break;
+        default:
+            printf("Insert the first grade of the %dth stundent: ", loop);
+            scanf("%f", &st_grade);
+            printf("Insert the second grade of the %dth stundent: ", loop);
+            scanf("%f", &nd_grade);
+            average = (st_grade + nd_grade)/2;
+            printf("The average is %.1f\n\n", average);
+            loop = loop+1;
+            break;
+        }
     }
 }
